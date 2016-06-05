52be29dcca56959a07ce67eb809f8de765b6b689
chinesejie <chinesejie@qq.com>
2016-06-05 13:19:39 +0800

    project init

---
+++ file_operations.h
@@ -0,0 +1,15 @@
+/*
+ * file_operations.h
+ *
+ *  Created on: 2015年2月5日
+ *      Author: Administrator
+ */
+
+#ifndef FILE_OPERATIONS_H_
+#define FILE_OPERATIONS_H_
+#include "g_net_global.h"
+
+int read_file_to_buff(char *file_path, unsigned long len, char *data);
+
+
+#endif /* FILE_OPERATIONS_H_ */
---
+++ g_net_update.h
@@ -0,0 +1,13 @@
+/*
+ * g_net_update.h
+ *
+ *  Created on: 2015年2月5日
+ *      Author: Administrator
+ */
+
+#ifndef G_NET_UPDATE_H_
+#define G_NET_UPDATE_H_
+#include "g_net_global.h"
+
+CONFIG_INFO *get_config_info(void);
+#endif /* G_NET_UPDATE_H_ */
